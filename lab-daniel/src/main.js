055e64c1fdeae4857f57ec26c2f9cce5d76ef706
DANIEL PILLAY <DPILLAY03@GMAIL.COM>
2018-04-19 20:14:11 -0700

    created user search form. it is logging user input on console.

--- lab-daniel/src/main.js
+++ lab-daniel/src/main.js
@@ -1,19 +1,23 @@
-'use strict'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchForm from './components/SearchForm';
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            title: 'Testing...'
+            title: 'Reddit Subreddit Search Engine'
         }
     }
 
     render() {
-        return <div>
+        return (
+        <div>
             <h1>{this.state.title}</h1>
+            <SearchForm />
         </div>
+        )
     }
 }
 
