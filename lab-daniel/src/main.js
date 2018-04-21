2bb9671452071e5730d944598e68e3c573af99c0
DANIEL PILLAY <DPILLAY03@GMAIL.COM>
2018-04-20 21:58:34 -0700

    committing what i have

--- lab-daniel/src/main.js
+++ lab-daniel/src/main.js
@@ -2,20 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchForm from './components/SearchForm';
+import SearchResultList from './components/SearchResultList';
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            title: 'Reddit Subreddit Search Engine'
+            title: 'Reddit Subreddit Search Engine',
+            results: ['Basketball', 'Baseball', 'Football']
         }
     }
 
+    performSearch(query) {
+        if(query === 'bjj') {
+            this.setState({results: ['Jiu-jitsu', 'Brazilian Jiu-jitsu', 'Artem Lobov']})
+        } else {
+            this.setState({results: ['Not found!']});
+        }
+    }
     render() {
         return (
         <div>
             <h1>{this.state.title}</h1>
             <SearchForm />
+            <SearchResultList />
         </div>
         )
     }
